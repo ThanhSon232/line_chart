ff453043897772f734db20cf3e52ec50457ef9d8
thanhson <trthanhson232@gmail.com>
2024-04-24 16:41:17 +0700

    add label

--- lib/chart/stv_line_chart.dart
+++ lib/chart/stv_line_chart.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:line_chart/model/stv_grid_chart_model.dart';
+import 'package:line_chart/model/stv_line_chart_data.dart';
 import 'package:line_chart/model/stv_line_chart_model.dart';
+import 'package:line_chart/model/stv_line_configure.dart';
 import 'package:line_chart/model/stv_tile_model.dart';
 
 part 'stv_chart_custom_paint.dart';
@@ -39,12 +39,14 @@ class STVLineChart extends StatelessWidget {
                   alignment: Alignment.centerRight,
                   children: [
                     LayoutBuilder(builder: (context, constraints) {
-                      return CustomPaint(
-                        foregroundPainter:
-                            _STVChartCustomPaint(lines, maxX, maxY),
-                        size: Size(
-                          constraints.maxWidth,
-                          constraints.maxHeight,
+                      return RepaintBoundary(
+                        child: CustomPaint(
+                          painter: _STVChartCustomPaint(lines, maxX, maxY),
+                          foregroundPainter: _STVTag(lines, maxX, maxY),
+                          size: Size(
+                            constraints.maxWidth,
+                            constraints.maxHeight,
+                          ),
                         ),
                       );
                     }),
@@ -79,5 +81,69 @@ class STVLineChart extends StatelessWidget {
         if (tiles?.bottomTile?.child != null) tiles!.bottomTile!.child,
       ],
     );
+  }
+}
+
+class _STVTag extends CustomPainter {
+  final List<STVLineChartModel> lines;
+  final double maxX;
+  final double maxY;
+
+  _STVTag(this.lines, this.maxX, this.maxY);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (int i = 0; i < lines.length; i++) {
+      STVLineChartData? data = lines[i].data.lastOrNull;
+      if (data != null &&
+          lines[i].configure.tagConfigure != null &&
+          lines[i].configure.tagConfigure!.showTag) {
+        var offset = Offset((data.x / maxX) * size.width,
+            size.height - (data.y / maxY) * size.height);
+        buildTag(size, canvas, offset, lines[i].configure.tagConfigure!);
+      }
+    }
+  }
+
+  void buildTag(
+      Size size, Canvas canvas, Offset offset, STVTagConfigure tagConfigure) {
+    var textPainter = TextPainter(
+      textDirection: TextDirection.ltr,
+    );
+
+    var textSpan = TextSpan(
+      text: tagConfigure.title,
+      style: tagConfigure.textStyle?.merge(
+        TextStyle(
+          foreground: Paint()
+            ..shader = tagConfigure.textGradient?.createShader(Rect.largest),
+        ),
+      ),
+    );
+    textPainter.text = textSpan;
+
+    textPainter.layout(
+      minWidth: 0,
+      maxWidth: size.width,
+    );
+
+    canvas.drawRect(
+        Rect.fromLTWH(
+            size.width - textPainter.width - tagConfigure.padding.left * 2,
+            offset.dy - textPainter.height - tagConfigure.padding.top,
+            textPainter.width + tagConfigure.padding.left * 2,
+            textPainter.height + tagConfigure.padding.top * 2),
+        Paint()..color = tagConfigure.backgroundColor);
+
+    textPainter.paint(
+      canvas,
+      Offset(size.width - (textPainter.width + tagConfigure.padding.left),
+          offset.dy - textPainter.height),
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
--- lib/model/stv_line_configure.dart
+++ lib/model/stv_line_configure.dart
@@ -4,10 +4,31 @@ class STVLineConfigure {
   final bool filled;
   final Color lineColor;
   final double lineWidth;
-
+  final STVTagConfigure? tagConfigure;
   STVLineConfigure({
     this.filled = false,
     this.lineColor = Colors.yellow,
     this.lineWidth = 2.0,
+    this.tagConfigure,
+  });
+}
+
+class STVTagConfigure {
+  final bool showTag;
+  final String? title;
+  final Color backgroundColor;
+  final LinearGradient? textGradient;
+  final LinearGradient? bgGradient;
+  final TextStyle? textStyle;
+  final EdgeInsets padding;
+
+  STVTagConfigure({
+    this.showTag = false,
+    this.title,
+    this.backgroundColor = Colors.black,
+    this.textGradient,
+    this.textStyle,
+    this.bgGradient,
+    this.padding = EdgeInsets.zero,
   });
 }
